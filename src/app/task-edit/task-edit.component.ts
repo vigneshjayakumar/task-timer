96b39671b092f9cb34d6a0adfe47c1e95d1cc33f
Vignesh Jayakumar <vigneshj@agrosperity.com>
2023-12-22 18:29:39 +0530

    Edit-Add-Form Completed

--- src/app/task-edit/task-edit.component.ts
+++ src/app/task-edit/task-edit.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-task-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './task-edit.component.html',
-  styleUrls: ['./task-edit.component.css']
+  styleUrls: ['./task-edit.component.css'],
 })
 export class TaskEditComponent {
+  taskForm: FormGroup;
 
+  constructor() {
+    this.taskForm = new FormGroup({
+      title: new FormControl('', Validators.required),
+      desc: new FormControl('', Validators.required),
+      targetTime: new FormControl(0, Validators.required),
+    });
+  }
+
+  submitTaskTime() {
+    if (!this.taskForm.valid) {
+      return;
+    }
+    console.log(this.taskForm.value);
+  }
 }
